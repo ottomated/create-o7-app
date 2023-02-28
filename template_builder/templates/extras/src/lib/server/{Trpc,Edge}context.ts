89225447804e3d984346db476233e86a9badb13b
Ottomated <otto@ottomated.net>
2023-02-27 22:31:26 -0800

    tRPC createContext

---
+++ template_builder/templates/extras/src/lib/server/{Trpc,Edge}context.ts
@@ -0,0 +1,13 @@
+import type { inferAsyncReturnType } from '@trpc/server';
+import type { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
+import type { RequestEvent } from '@sveltejs/kit';
+
+export function createContext(event: RequestEvent) {
+	return (opts: FetchCreateContextFnOptions) => {
+		return {
+			env: event.platform?.env,
+			context: event.platform?.context,
+		};
+	};
+}
+export type Context = inferAsyncReturnType<inferAsyncReturnType<typeof createContext>>;
