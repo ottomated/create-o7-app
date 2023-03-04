9d15d680790e822971657209c22da9eb6a3317d2
Ottomated <otto@ottomated.net>
2023-03-03 20:26:33 -0800

    remove dependency on binary-install

--- pkg/bin.js
+++ pkg/bin.js
@@ -1,7 +1,4 @@
-
-const { Binary } = require('binary-install');
 const os = require('os');
-const { version } = require('./package.json');
 
 const platforms = [
 	{
@@ -42,7 +39,52 @@ if (!supported) {
 	);
 }
 
-module.exports = new Binary(
-	'create-o7-app',
-	`https://github.com/ottomated/create-o7-app/releases/download/${version}/create-o7-app-${supported.file}.tar.gz`
-);
+const { join } = require('path');
+const { existsSync, readdirSync, unlinkSync, mkdirSync } = require('fs');
+const dir = join(__dirname, "node_modules", ".bin");
+const bin = join(dir, `create-o7-app-${supported.file}`);
+
+const exists = existsSync(bin);
+
+async function install() {
+	if (exists) return;
+
+	if (!existsSync(dir)) {
+		mkdirSync(dir, { recursive: true });
+	}
+
+	const { version } = require('./package.json');
+	const res = await require('axios')({
+		url: `https://github.com/ottomated/create-o7-app/releases/download/${version}/create-o7-app-${supported.file}.tar.gz`,
+		responseType: 'stream'
+	}).catch(e => {
+		console.error(`Error fetching release: ${e.message}`);
+		process.exit(1);
+	});
+	return new Promise((resolve, reject) => {
+		const sink = res.data.pipe(
+			require('tar').x({ strip: 1, C: dir })
+		);
+		sink.on('finish', () => resolve());
+		sink.on('error', err => {
+			console.error(`Error fetching release: ${err.message}`);
+			process.exit(1);
+		});
+	});
+}
+
+async function run() {
+	if (!exists) await install();
+	const args = process.argv.slice(2);
+	const child = require('child_process').spawnSync(bin, args, {
+		cwd: process.cwd(), stdio: 'inherit'
+	});
+	if (child.error) {
+		console.error(child.error);
+		child.exit(1);
+	}
+	process.exit(child.status);
+}
+
+
+module.exports = { install, run };
