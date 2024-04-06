bcaa4d582b02b9af65beb78eee627abdfdcf873f
Ottomated <otto@ottomated.net>
2024-04-05 20:30:53 -0700

    sort package.json keys

--- src/create/package_json.rs
+++ src/create/package_json.rs
@@ -6,26 +6,27 @@ use serde_json::{ser::PrettyFormatter, Serializer};
 use std::fs;
 
 pub fn create_package_json(input: &UserInput) -> Result<()> {
-	let (mut base, extras) = templates::get_package_jsons();
+	let (base, extras) = templates::get_package_jsons();
+	let mut package_json = base.contents;
 
-	for package_json in extras {
-		let included = match &package_json.features {
+	for extra in extras {
+		let included = match &extra.features {
 			Some(features) => features.is_subset(&input.features),
 			None => true,
 		};
 		if !included {
 			continue;
 		}
-		base.contents.merge(package_json.contents);
+		package_json.merge(extra.contents);
 	}
 
-	base.contents.name = Some(&input.location.name);
+	package_json.name = Some(&input.location.name);
 
 	let target_path = &input.location.path.join("package.json");
 	let formatter = PrettyFormatter::with_indent(b"\t");
 	let buf = Vec::new();
 	let mut ser = Serializer::with_formatter(buf, formatter);
-	base.contents
+	package_json
 		.serialize(&mut ser)
 		.context("Failed to serialize package.json")?;
 
