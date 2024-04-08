1e7fa253ff59d34aaa2fb001df8848a4950023a1
Ottomated <otto@ottomated.net>
2024-04-07 22:31:11 -0700

    feat: Insert app name in app.html and wrangler.toml

--- src/create/scaffold.rs
+++ src/create/scaffold.rs
@@ -3,8 +3,14 @@ use crate::input::UserInput;
 use anyhow::{Context, Result};
 use std::fs;
 
+const REPLACABLE_EXTENSIONS: [&str; 2] = ["html", "toml"];
+const NAME_PLACEHOLDER: &[u8] = b"__o7__name__";
+
 pub fn scaffold(input: &UserInput) -> Result<()> {
 	let templates = templates::get_templates();
+
+	let name_replacement = input.location.name.as_bytes();
+
 	for template in templates {
 		let included = match template.features {
 			Some(features) => features.is_subset(&input.features),
@@ -19,7 +25,29 @@ pub fn scaffold(input: &UserInput) -> Result<()> {
 			fs::create_dir_all(folder)
 				.with_context(|| format!("Could not create {}", folder.display()))?;
 		}
-		fs::write(target_path, template.contents)
+
+		let extension = target_path.extension().and_then(|e| e.to_str());
+
+		let contents = match extension {
+			Some(ext) if REPLACABLE_EXTENSIONS.contains(&ext) => {
+				let mut result = template.contents.to_vec();
+
+				if let Some(pos) = template
+					.contents
+					.windows(NAME_PLACEHOLDER.len())
+					.position(|window| window == NAME_PLACEHOLDER)
+				{
+					result.splice(
+						pos..pos + NAME_PLACEHOLDER.len(),
+						name_replacement.iter().cloned(),
+					);
+				}
+
+				result
+			}
+			_ => template.contents.to_vec(),
+		};
+		fs::write(target_path, contents)
 			.with_context(|| format!("Could not write {}", template.path))?;
 	}
 
