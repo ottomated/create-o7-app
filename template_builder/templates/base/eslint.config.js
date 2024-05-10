ab92c5036084af4a85e31cc9ee957d967cc7b173
Ottomated <otto@ottomated.net>
2024-05-10 15:21:56 -0700

    feat: Update to eslint v9

---
+++ template_builder/templates/base/eslint.config.js
@@ -0,0 +1,59 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintPluginSvelte from 'eslint-plugin-svelte';
+import tseslint from 'typescript-eslint';
+
+const svelteParser = await import(
+	eslintPluginSvelte.configs.base.overrides[0].parser
+);
+
+export default tseslint.config(
+	js.configs.recommended,
+	...tseslint.configs.recommended,
+	...eslintPluginSvelte.configs['flat/recommended'],
+	...eslintPluginSvelte.configs['flat/prettier'],
+	prettierRecommended,
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.es2017,
+			},
+		},
+	},
+	{
+		files: ['**/*.cjs'],
+		languageOptions: {
+			globals: {
+				...globals.node,
+			},
+		},
+	},
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parser: svelteParser,
+			parserOptions: {
+				parser: tseslint.parser,
+			},
+		},
+	},
+	{
+		files: ['**/*.ts'],
+		rules: {
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{
+					argsIgnorePattern: '^_',
+				},
+			],
+		},
+	},
+	{
+		rules: {
+			'prettier/prettier': 'warn',
+		},
+	},
+	{ ignores: ['**/.svelte-kit'] },
+);
