32315d82b10d0c27497ccfa6776ac3a54ad52841
Ottomated <otto@ottomated.net>
2024-06-18 12:39:48 -0700

    feat: Use svelte 5 syntax in svelte 5 template

---
+++ template_builder/templates/extras/src/routes/{Trpc,Svelte5}+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../app.css';
+	import { QueryClientProvider } from '@tanstack/svelte-query';
+	import { trpc } from '$lib/trpc';
+
+	const { data, children } = $props();
+
+	const queryClient = trpc.hydrateFromServer(data.trpc);
+</script>
+
+<QueryClientProvider client={queryClient}>
+	{@render children()}
+</QueryClientProvider>
