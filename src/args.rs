b5bdcd23e698ea459e70cf6589365db2bf8cc8b1
Ottomated <otto@ottomated.net>
2024-07-16 16:17:33 -0700

    feat: Enable/disable telemetry

---
+++ src/args.rs
@@ -0,0 +1,24 @@
+use anyhow::Context;
+use clap::Parser;
+
+use crate::utils::PackageManager;
+
+#[derive(clap::Parser, Debug)]
+#[command(version, author, long_about = None)]
+pub struct Args {
+	/// Override the automatic package manager detection
+	#[arg(value_enum, long)]
+	pub package_manager: Option<PackageManager>,
+
+	/// Turn off telemetry reporting
+	#[arg(long)]
+	pub disable_telemetry: bool,
+
+	/// Turn on telemetry reporting
+	#[arg(long)]
+	pub enable_telemetry: bool,
+}
+
+pub fn parse() -> Args {
+	Args::parse()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod args;
 mod create;
 mod input;
 #[cfg(test)]
@@ -5,10 +6,30 @@ mod test;
 mod utils;
 
 use anyhow::Result;
+use crossterm::style::{style, Stylize};
+use utils::get_package_manager;
 
 use crate::create::create;
 
 fn main() -> Result<()> {
+	let arguments = args::parse();
+
+	if arguments.disable_telemetry {
+		let package_manager = get_package_manager();
+		let enable_command = format!("{package_manager} create o7-app --enable-telemetry");
+		println!();
+		println!("Telemetry disabled :(");
+		println!("Run '{}' to re-enable", style(enable_command).green());
+		println!();
+		std::process::exit(0);
+	}
+	if arguments.enable_telemetry {
+		println!();
+		println!("Telemetry enabled :)");
+		println!();
+		std::process::exit(0);
+	}
+
 	let input = input::prompt()?;
 
 	create(input)?;
