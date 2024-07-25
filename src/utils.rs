275839fab8b8348a5052507a17efd2c1ae5d9681
ErickCReis <erickcorreareis@gmail.com>
2024-07-24 23:46:21 -0300

    run_all test

--- src/utils.rs
+++ src/utils.rs
@@ -140,3 +140,54 @@ fn merge_hashmaps<'a>(
 	}
 	old.retain(|_, v| v.is_some());
 }
+
+pub fn generate_combinations() -> Vec<HashSet<Feature>> {
+	let features = get_feature_list();
+	let mut last_step = vec![HashSet::new()];
+
+	for i in 0..features.len() {
+		let feature_details = features.get(i).unwrap();
+		let mut next = vec![];
+		for past in last_step {
+			match feature_details {
+				FeatureDetails::Boolean(details) => {
+					let (show, value) = details.should_show(&past);
+					if !show {
+						let mut set = past.clone();
+						if value {
+							set.insert(details.feature);
+						}
+						next.push(set);
+					} else {
+						let mut yes = past.clone();
+						yes.insert(details.feature);
+						let no = past;
+						next.push(yes);
+						next.push(no);
+					}
+				}
+				FeatureDetails::Option(details) => {
+					let show = details.should_show(&past);
+					if !show {
+						next.push(past);
+						continue;
+					}
+					for option in details.options.iter() {
+						if !option.should_show(&past) {
+							continue;
+						}
+						let mut set = past.clone();
+						if let Some(feature) = option.feature {
+							set.insert(feature);
+						}
+						next.push(set);
+					}
+				}
+			}
+		}
+
+		last_step = next;
+	}
+
+	last_step
+}
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -49,88 +49,8 @@ fn test_pnpm(dir: &PathBuf, args: &[&'static str]) -> Result<Output, String> {
 	Ok(output)
 }
 
-// fn run_all() {
-// 	let mut combinations = generate_combinations(get_feature_list());
-// 	// let mut combinations = vec![HashSet::new()];
-// 	// combinations[0].insert(Feature::Edge);
-// 	// combinations[0].insert(Feature::D1);
-// 	// combinations[0].insert(Feature::Trpc);
-
-// 	let num_threads = min(
-// 		{
-// 			let possible =
-// 				usize::from(available_parallelism().unwrap_or(NonZeroUsize::new(1).unwrap()));
-
-// 			if std::env::var("CI").is_ok() {
-// 				if possible == 1 {
-// 					1usize
-// 				} else {
-// 					possible - 1
-// 				}
-// 			} else {
-// 				possible / 2
-// 			}
-// 		},
-// 		combinations.len(),
-// 	);
-
-// 	println!(
-// 		"Testing {} combinations on {num_threads} threads",
-// 		combinations.len()
-// 	);
-
-// 	let mut chunks = vec![vec![]; num_threads];
-// 	while !combinations.is_empty() {
-// 		#[allow(clippy::needless_range_loop)]
-// 		for i in 0..num_threads {
-// 			let Some(c) = combinations.pop() else {
-// 				break;
-// 			};
-// 			chunks[i].push(c);
-// 		}
-// 	}
-
-// 	thread::scope(|s| {
-// 		let errors = Arc::new(RwLock::new(vec![]));
-// 		let thread_count = Arc::new(AtomicU16::new(0));
-
-// 		for chunk in chunks {
-// 			thread_count.fetch_add(1, Ordering::SeqCst);
-// 			let errors = Arc::clone(&errors);
-// 			let thread_count = Arc::clone(&thread_count);
-
-// 			s.spawn(move || {
-// 				for features in chunk {
-// 					let result = run_feature(features.clone());
-// 					if let Err(e) = result {
-// 						errors.write().unwrap().push((features.clone(), e));
-// 					}
-// 				}
-// 				thread_count.fetch_sub(1, Ordering::SeqCst);
-// 			});
-// 		}
-// 		while thread_count.load(Ordering::SeqCst) > 0 {
-// 			thread::sleep(Duration::from_millis(1));
-// 		}
-// 		let errors = errors.read().unwrap();
-// 		if !errors.is_empty() {
-// 			panic!(
-// 				"{} errors occurred:\n\n{}",
-// 				errors.len(),
-// 				errors
-// 					.iter()
-// 					.map(|(features, e)| {
-// 						format!("Error with features {:?}:\n\n{}\n\n", features, e)
-// 					})
-// 					.join("\n")
-// 			);
-// 		}
-// 	});
-// }
-
 pub fn run_feature(features: HashSet<Feature>) -> Result<(), String> {
 	let input = make_input(features);
-	println!("input: {:?}", input);
 
 	let dir = input.location.path.clone();
 	let result: Result<(), String> = (|| {
