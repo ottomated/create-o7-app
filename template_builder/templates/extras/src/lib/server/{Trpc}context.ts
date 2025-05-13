f3a52ec7f0ebfcbfb298bf7e20541b9923ad4b5e
Ottomated <otto@ottomated.net>
2025-05-13 12:45:20 -0700

    feat: replace deprecated inferAsyncReturnType

--- template_builder/templates/extras/src/lib/server/{Trpc}context.ts
+++ template_builder/templates/extras/src/lib/server/{Trpc}context.ts
@@ -1,8 +1,7 @@
 import type { RequestEvent } from '@sveltejs/kit';
-import type { inferAsyncReturnType } from '@trpc/server';
 
 export async function createContext(_event: RequestEvent) {
 	return {};
 }
 
-export type Context = inferAsyncReturnType<typeof createContext>;
+export type Context = Awaited<ReturnType<typeof createContext>>;
