731a79e62924813f6fa91bf6dfea1a8af1d53182
Ottomated <otto@ottomated.net>
2025-05-29 14:39:09 -0700

    svelte.config.js

--- template_builder/templates/base/eslint.config.js
+++ template_builder/templates/base/eslint.config.js
@@ -3,7 +3,7 @@ import js from '@eslint/js';
 import prettier from 'eslint-plugin-prettier/recommended';
 import svelte from 'eslint-plugin-svelte';
 import ts from 'typescript-eslint';
-import svelteConfig from './svelte.config';
+import svelteConfig from './svelte.config.js';
 
 export default ts.config(
 	js.configs.recommended,
